16f0d3ceddfda2ca2e180f552d9d24ba5aabab07
Gabriela Miranda <mm15136@ues.edu.sv>
2025-04-20 21:59:04 -0600

    Entitys añadidas

---
+++ src/entity/UserRoles.ts
@@ -0,0 +1,22 @@
+import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "./User";
+import { Role } from "./Role";
+@Entity()
+export class UserRoles {
+    @PrimaryGeneratedColumn("increment",{type:"bigint"})
+    id: number| undefined;
+    @ManyToOne(()=> User, (user)=> user.userRoles, {
+    nullable:false,
+    onDelete:"RESTRICT",
+    onUpdate:"CASCADE"
+    })
+    @JoinColumn({name:"user_id"})
+    user: User | undefined;
+    @ManyToOne(()=> Role, (role)=> role.userRoles, {
+    nullable:false,
+    onDelete:"RESTRICT",
+    onUpdate:"CASCADE"
+    })
+    @JoinColumn({name:"role_id"})
+    role: Role | undefined;
+}
